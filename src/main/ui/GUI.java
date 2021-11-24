4b9954772110304b6cd4df8cca5a13c03c035923
Gordon Lim <klgordon@students.cs.ubc.ca>
2021-11-23 17:35:40 -0800

    Implemented edit and delete functionalities in GUI

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -1,18 +1,12 @@
 package ui;
 
-import com.sun.javafx.fxml.LoadListener;
 import model.Person;
 import model.PersonList;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.plaf.basic.BasicOptionPaneUI;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,7 +17,7 @@ public class GUI {
     private PersonList personList = new PersonList();
 
     @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
-    public GUI() throws IOException {
+    public GUI() {
         // set up JFrame
         JFrame frame = new JFrame("NamesDB");
         frame.setVisible(true);
@@ -39,20 +33,13 @@ public class GUI {
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         String[] columnNames = {"Name", "Gender", "Hair Color", "Place of Encounter"};
-        String[][] data = {
-                { "Monica", "Female", "Brown", "Central Perk"},
-                { "Jeff", "Male", "Brown", "Greendale" },
-        };
-        Person kundanKumarJha = new Person("Kundan Kumar Jha", "4031", "CSE", "o");
-        Person anandJha = new Person("Anand Jha", "6014", "IT", "b");
-        personList.addPerson(kundanKumarJha);
-        personList.addPerson(anandJha);
+        String[][] data = {};
+
         DefaultTableModel tableModel = new DefaultTableModel(data, columnNames);
         JTable table = new JTable(tableModel);
 
         // add the jTable to the scrollPane, and the scrollPane to the JPanel
         JScrollPane scrollPane = new JScrollPane(table);
-        gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
         panel.add(scrollPane, gridBagConstraints);
 
@@ -74,100 +61,125 @@ public class GUI {
         buttonPanel.add(deleteButton);
 
         // action for loadButton
-        loadButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JsonReader jsonReader = new JsonReader("./data/personList.json");
-                try {
-                    personList = jsonReader.read();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-                String[][] newPersonArray = new String[personList.getPersonList().size()][4];
-                int row = 0;
-                for (Person person : personList.getPersonList()) {
-                    newPersonArray[row][0] = person.getName();
-                    newPersonArray[row][1] = person.getGender();
-                    newPersonArray[row][2] = person.getHairColor();
-                    newPersonArray[row][3] = person.getWhereMet();
-                    row++;
-                }
-                tableModel.setRowCount(row);
-                for (int row2 = 0; row2 < row; row2++) {
-                    for (int col = 0; col < 4; col++) {
-                        tableModel.setValueAt(newPersonArray[row2][col], row2, col);
-                    }
+        loadButton.addActionListener(e -> {
+            JsonReader jsonReader = new JsonReader("./data/personList.json");
+            try {
+                personList = jsonReader.read();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            String[][] newPersonArray = new String[personList.getPersonList().size()][4];
+            int row = 0;
+            for (Person person : personList.getPersonList()) {
+                newPersonArray[row][0] = person.getName();
+                newPersonArray[row][1] = person.getGender();
+                newPersonArray[row][2] = person.getHairColor();
+                newPersonArray[row][3] = person.getWhereMet();
+                row++;
+            }
+            tableModel.setRowCount(row);
+            for (int row2 = 0; row2 < row; row2++) {
+                for (int col = 0; col < 4; col++) {
+                    tableModel.setValueAt(newPersonArray[row2][col], row2, col);
                 }
             }
         });
 
         // action for saveButton
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JsonWriter jsonWriter = new JsonWriter("./data/personList.json");
-                try {
-                    jsonWriter.open();
-                } catch (FileNotFoundException ex) {
-                    ex.printStackTrace();
-                }
-                jsonWriter.write(personList);
-                jsonWriter.close();
+        saveButton.addActionListener(e -> {
+            JsonWriter jsonWriter = new JsonWriter("./data/personList.json");
+            try {
+                jsonWriter.open();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
             }
+            jsonWriter.write(personList);
+            jsonWriter.close();
         });
 
         // action for addButton
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame inputFrame = new JFrame();
-                String name = JOptionPane.showInputDialog(inputFrame,"Enter Name");
-                String gender = JOptionPane.showInputDialog(inputFrame,"Enter Gender");
-                String hairColor = JOptionPane.showInputDialog(inputFrame,"Enter Hair Color");
-                String whereMet = JOptionPane.showInputDialog(inputFrame,"Enter Place of Encounter");
-                Person newPerson = new Person(name, gender, hairColor, whereMet);
-                personList.addPerson(newPerson);
+        addButton.addActionListener(e -> {
+            JFrame inputFrame = new JFrame();
+            String name = JOptionPane.showInputDialog(inputFrame,"Enter Name");
+            String gender = JOptionPane.showInputDialog(inputFrame,"Enter Gender");
+            String hairColor = JOptionPane.showInputDialog(inputFrame,"Enter Hair Color");
+            String whereMet = JOptionPane.showInputDialog(inputFrame,"Enter Place of Encounter");
+            Person newPerson = new Person(name, gender, hairColor, whereMet);
+            personList.addPerson(newPerson);
 
-                String[][] newPersonArray = new String[personList.getPersonList().size()][4];
-                int row = 0;
-                for (Person person : personList.getPersonList()) {
-                    newPersonArray[row][0] = person.getName();
-                    newPersonArray[row][1] = person.getGender();
-                    newPersonArray[row][2] = person.getHairColor();
-                    newPersonArray[row][3] = person.getWhereMet();
-                    row++;
-                }
-                tableModel.setRowCount(row);
-                for (int row2 = 0; row2 < row; row2++) {
-                    for (int col = 0; col < 4; col++) {
-                        tableModel.setValueAt(newPersonArray[row2][col], row2, col);
-                    }
+            String[][] newPersonArray = new String[personList.getPersonList().size()][4];
+            int row = 0;
+            for (Person person : personList.getPersonList()) {
+                newPersonArray[row][0] = person.getName();
+                newPersonArray[row][1] = person.getGender();
+                newPersonArray[row][2] = person.getHairColor();
+                newPersonArray[row][3] = person.getWhereMet();
+                row++;
+            }
+            tableModel.setRowCount(row);
+            for (int row2 = 0; row2 < row; row2++) {
+                for (int col = 0; col < 4; col++) {
+                    tableModel.setValueAt(newPersonArray[row2][col], row2, col);
                 }
             }
         });
 
-        // action for clicking on a cell
-        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                // action for editButton
-                editButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        // TODO: need to know how to delete a person from personList, given the selected row
-                    }
-                });
+        // action for editButton
+        editButton.addActionListener(e -> {
+            if (table.getSelectedRow() < 0) {
+                return;
             }
+            for (Person person : personList.getPersonList()) {
+                boolean sameName =
+                        tableModel.getValueAt(table.getSelectedRow(), 0).equals(person.getName());
+                boolean sameGender =
+                        tableModel.getValueAt(table.getSelectedRow(), 1).equals(person.getGender());
+                boolean sameHairColor =
+                        tableModel.getValueAt(table.getSelectedRow(), 2).equals(person.getHairColor());
+                boolean sameWhereMet =
+                        tableModel.getValueAt(table.getSelectedRow(), 3).equals(person.getWhereMet());
+                if (sameName && sameGender && sameHairColor && sameWhereMet) {
+                    JFrame inputFrame = new JFrame();
+                    String name = JOptionPane.showInputDialog(inputFrame,"Enter Name");
+                    String gender = JOptionPane.showInputDialog(inputFrame,"Enter Gender");
+                    String hairColor = JOptionPane.showInputDialog(inputFrame,"Enter Hair Color");
+                    String whereMet = JOptionPane.showInputDialog(inputFrame,"Enter Place of Encounter");
+                    person.modifyPerson(name, gender, hairColor, whereMet);
+                    tableModel.setValueAt(name, table.getSelectedRow(), 0);
+                    tableModel.setValueAt(gender, table.getSelectedRow(), 1);
+                    tableModel.setValueAt(hairColor, table.getSelectedRow(), 2);
+                    tableModel.setValueAt(whereMet, table.getSelectedRow(), 3);
+                    break;
+                }
+            }
+        });
 
+        // action for deleteButton
+        deleteButton.addActionListener(e -> {
+            if (table.getSelectedRow() < 0) {
+                return;
+            }
+            Person personToBeRemoved = null;
+            for (Person person : personList.getPersonList()) {
+                boolean sameName =
+                        tableModel.getValueAt(table.getSelectedRow(), 0).equals(person.getName());
+                boolean sameGender =
+                        tableModel.getValueAt(table.getSelectedRow(), 1).equals(person.getGender());
+                boolean sameHairColor =
+                        tableModel.getValueAt(table.getSelectedRow(), 2).equals(person.getHairColor());
+                boolean sameWhereMet =
+                        tableModel.getValueAt(table.getSelectedRow(), 3).equals(person.getWhereMet());
+                if (sameName && sameGender && sameHairColor && sameWhereMet) {
+                    personToBeRemoved = person;
+                    break;
+                }
+            }
+            personList.deletePerson(personToBeRemoved);
+            tableModel.removeRow(table.getSelectedRow());
         });
 
         // finalize the JFrame
         frame.pack();
         frame.setSize(600, 600);
     }
-
-    private void updateTableModel(TableModel tableModel, PersonList personList) {
-
-    }
-
 }
